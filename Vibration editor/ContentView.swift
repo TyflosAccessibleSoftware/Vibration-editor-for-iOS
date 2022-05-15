d7d53d827dbdeaeb46858ef4cd608d57c3bb666c
Jonathan Chacón <jonathan.chacon@cabify.com>
2022-05-15 05:32:02 +0200

    Initial Commit

---
+++ Vibration editor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vibration editor
+//
+//  Created by Jonathan Chacón Barbero on 15/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vibration editor/Vibration_editorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vibration_editorApp.swift
+//  Vibration editor
+//
+//  Created by Jonathan Chacón Barbero on 15/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Vibration_editorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
